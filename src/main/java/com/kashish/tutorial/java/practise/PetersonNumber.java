bcf62a80439d1b3c085c0161535cec3759d24fb2
Coder Shaira <kangiakashish2001@gmail.com>
2022-09-19 16:40:03 +0530

    this is peterson number code.

--- src/main/java/com/kashish/tutorial/java/practise/PetersonNumber.java
+++ src/main/java/com/kashish/tutorial/java/practise/PetersonNumber.java
@@ -8,6 +8,31 @@ public class PetersonNumber {
 Scanner scanner=new Scanner(System.in);
 int number=scanner.nextInt();
 
-	}
+int num=number;
+int count=0;
+while(number!=0) {
+	int rem=number%10;
+	int fact=1;
+	
+while(rem!=0) {
+	 fact=fact*rem;
+	
+	--rem;
+	
+}
+count=count+fact;
+
+number=number/10;
 
 }
+System.out.println(count);
+if(count==num)
+	System.out.print("It is a peterson number");
+else
+	System.out.print("It is not a peterson number");
+}
+	
+		
+	}
+
+
