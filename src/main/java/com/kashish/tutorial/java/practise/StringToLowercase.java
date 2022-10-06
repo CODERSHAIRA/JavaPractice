d9eb4408a1ce45ae9fbcfd5e675c4e8afe4ec760
Coder Shaira <kangiakashish2001@gmail.com>
2022-10-06 19:49:31 +0530

    added string to lowercase code.

---
+++ src/main/java/com/kashish/tutorial/java/practise/StringToLowercase.java
@@ -0,0 +1,13 @@
+package com.kashish.tutorial.java.practise;
+
+public class StringToLowercase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//	String s="SHAIRA";
+		String s=new String("SHAIRA");
+		System.out.println(s.toLowerCase());
+	}
+
+}
