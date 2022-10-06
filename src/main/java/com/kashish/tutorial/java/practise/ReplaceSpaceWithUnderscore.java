04dae55d91382fed2e21f71a5fd3cd02e60fd485
Coder Shaira <kangiakashish2001@gmail.com>
2022-10-06 20:30:05 +0530

    added replace space with underscores code.

---
+++ src/main/java/com/kashish/tutorial/java/practise/ReplaceSpaceWithUnderscore.java
@@ -0,0 +1,12 @@
+package com.kashish.tutorial.java.practise;
+
+public class ReplaceSpaceWithUnderscore {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Hi Shaira";
+		System.out.println(s.replace(" ","_"));
+
+	}
+
+}
--- src/main/java/com/kashish/tutorial/java/practise/StringToLowercase.java
+++ src/main/java/com/kashish/tutorial/java/practise/StringToLowercase.java
@@ -3,9 +3,10 @@ package com.kashish.tutorial.java.practise;
 public class StringToLowercase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	//	String s="SHAIRA";
+	// TODO Auto-generated method stub
+       // First method 
+	    //	String s="SHAIRA";
+	//Second method
 		String s=new String("SHAIRA");
 		System.out.println(s.toLowerCase());
 	}
