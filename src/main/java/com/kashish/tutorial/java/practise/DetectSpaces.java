ad44c14af10d383ad4fbdd7b3b009dbbf6537803
Coder Shaira <kangiakashish2001@gmail.com>
2022-10-06 22:22:47 +0530

    added detect spaces.

---
+++ src/main/java/com/kashish/tutorial/java/practise/DetectSpaces.java
@@ -0,0 +1,13 @@
+package com.kashish.tutorial.java.practise;
+
+public class DetectSpaces {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="ae io u";
+System.out.println(s.indexOf(" "));
+System.out.println(s.indexOf("  "));
+
+	}
+
+}
