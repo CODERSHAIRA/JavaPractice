db0b66239364cf739e81857228402d5430361c05
Coder Shaira <kangiakashish2001@gmail.com>
2022-10-06 20:46:46 +0530

    added replace code.

---
+++ src/main/java/com/kashish/tutorial/java/practise/ReplaceCode.java
@@ -0,0 +1,11 @@
+package com.kashish.tutorial.java.practise;
+
+public class ReplaceCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String letter= "Dear Shaira thanks a lot";
+System.out.println(letter.replace("Shaira","Shaira Khan"));
+	}
+
+}
