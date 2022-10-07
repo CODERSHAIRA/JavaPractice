9e29a7d18ff52f051708eeb29b764e20fd799f32
Coder Shaira <kangiakashish2001@gmail.com>
2022-10-07 14:49:42 +0530

    added reverse multiplication table code.

---
+++ src/main/java/com/kashish/tutorial/java/practise/MultiplicationInReverseOrder.java
@@ -0,0 +1,13 @@
+package com.kashish.tutorial.java.practise;
+
+public class MultiplicationInReverseOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=10; i>=1; i--) {
+			System.out.print(10+"x"+i+ "=" +10*i);
+			System.out.print("\n");
+		}
+	}
+
+}
