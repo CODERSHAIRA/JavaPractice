8f181e6f8de0e12a43bf2649bdd98c7079558ea3
Coder Shaira <kangiakashish2001@gmail.com>
2022-10-07 15:30:24 +0530

    added factorial code.

---
+++ src/main/java/com/kashish/tutorial/java/practise/Factorial.java
@@ -0,0 +1,14 @@
+package com.kashish.tutorial.java.practise;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int fact=1;
+for(int i=4; i>=1; i--) {
+	fact=fact*i;
+}
+System.out.print(fact);
+	}
+
+}
