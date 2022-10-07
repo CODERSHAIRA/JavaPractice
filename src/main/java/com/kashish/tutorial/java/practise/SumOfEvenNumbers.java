51dc0c20d4725b88312f23627796e2cdc30ece8d
Coder Shaira <kangiakashish2001@gmail.com>
2022-10-07 14:18:07 +0530

    added sum of even numbers code.

---
+++ src/main/java/com/kashish/tutorial/java/practise/SumOfEvenNumbers.java
@@ -0,0 +1,21 @@
+package com.kashish.tutorial.java.practise;
+
+public class SumOfEvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=1;
+	
+		int sum=0;
+while(num<5) {
+	if(num%2==0) {
+	sum=sum+num;
+	num++;
+	}
+	else
+		num++;
+}
+System.out.print(sum);
+	}
+
+}
