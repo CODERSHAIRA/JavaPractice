dae1c2bff9235781e5edaf53098e05a2d77b7090
Coder Shaira <kangiakashish2001@gmail.com>
2022-10-10 14:44:17 +0530

    added java method code.

---
+++ src/main/java/com/kashish/tutorial/java/practise/JavaMethod.java
@@ -0,0 +1,15 @@
+package com.kashish.tutorial.java.practise;
+
+public class JavaMethod {
+
+	static void multiplication(int n) {
+		for(int i=1; i<=10; i++) {
+		System.out.format("%d X %d=%d\n",n,i,n*i);
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+multiplication(7);
+	}
+
+}
