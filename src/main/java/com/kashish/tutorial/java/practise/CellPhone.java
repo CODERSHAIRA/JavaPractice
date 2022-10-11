45a801b81bdbf5155171627b012b11a704a393ff
Coder Shaira <kangiakashish2001@gmail.com>
2022-10-12 01:16:13 +0530

    added oops question2.

---
+++ src/main/java/com/kashish/tutorial/java/practise/CellPhone.java
@@ -0,0 +1,18 @@
+package com.kashish.tutorial.java.practise;
+
+public class CellPhone {
+
+	public void ring() {
+		System.out.println("the cellphone is ringing");
+	}
+	public void vibrate() {
+		System.out.println("the cellphone is vibrating");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+CellPhone samsung =new CellPhone();
+samsung.ring();
+samsung.vibrate();
+	}
+
+}
