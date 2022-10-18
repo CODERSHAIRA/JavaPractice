12ec881f1f2c520a1d4cf443280c13b43094035e
Coder Shaira <kangiakashish2001@gmail.com>
2022-10-18 15:26:32 +0530

    added a access modifier code.

---
+++ src/main/java/calc/OfParent.java
@@ -0,0 +1,16 @@
+package calc;
+
+import com.kashish.tutorial.java.practise.Parent;
+
+
+public class OfParent extends Parent {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OfParent p=new OfParent();
+		System.out.println(p.parent1);
+		System.out.println(parent2);
+	}
+
+}
