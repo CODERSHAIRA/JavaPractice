c44f705d9b676672d3cad3530e01fc3a9815f940
Coder Shaira <kangiakashish2001@gmail.com>
2022-10-21 16:23:40 +0530

    added a thread code 2.

---
+++ src/main/java/com/kashish/tutorial/java/practise/ThreadRunnable.java
@@ -0,0 +1,39 @@
+package com.kashish.tutorial.java.practise;
+
+ class ThreadRunnable1 implements Runnable{
+	
+	public void run() {
+		int i=0;
+		while(i<100) {
+	System.out.println("I am a thread not a threat");	
+	i++;
+	}
+	}
+}
+
+ class ThreadRunnable2 implements Runnable{
+	
+	 
+	public void run() {
+		int i=0;
+		while(i<100) {
+	
+		System.out.println(" Ok i got it that,i am a thread not a threat");	
+		i++;
+	}
+	}
+}
+
+
+public class ThreadRunnable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ ThreadRunnable1 tr1 =new  ThreadRunnable1();
+Thread t1=new Thread(tr1);
+ThreadRunnable2 tr2=new ThreadRunnable2();
+Thread t2=new Thread(tr2);
+t1.start();
+t2.start();
+	}
+}
