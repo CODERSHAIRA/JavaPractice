da168e43535fd6fe524568b784eb8414719521c3
Coder Shaira <kangiakashish2001@gmail.com>
2022-11-15 15:28:32 +0530

    added a ASCII value code.

---
+++ src/main/java/com/kashish/tutorial/java/practise/Ascii_Value.java
@@ -0,0 +1,18 @@
+package com.kashish.tutorial.java.practise;
+
+public class Ascii_Value {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ char ch1='a';
+ char ch2='c';
+ 
+ int ascii1=(int)ch1;
+ int ascii2=(int)ch2;
+ 
+ System.out.println(ascii1);
+ System.out.println(ascii2);
+
+	}
+
+}
