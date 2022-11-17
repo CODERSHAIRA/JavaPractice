4f2790f1382333be03e62ee3412ab3de3fe665f7
Coder Shaira <kangiakashish2001@gmail.com>
2022-11-17 19:26:24 +0530

    added a sum of elements of an array.

---
+++ src/main/java/com/kashish/tutorial/java/practise/SumOfElementsOfArray.java
@@ -0,0 +1,17 @@
+package com.kashish.tutorial.java.practise;
+
+public class SumOfElementsOfArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int[] arr= {3,5,9};
+     int sum=0;
+     
+     for(int i=0; i<arr.length; i++) {
+    	 sum=sum+arr[i];
+  
+     }
+  	 System.out.print(sum);
+	}
+
+}
