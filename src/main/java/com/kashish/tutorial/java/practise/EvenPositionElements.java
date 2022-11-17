065832730d66dc2a582952b7ef107812ecea1305
Coder Shaira <kangiakashish2001@gmail.com>
2022-11-17 19:17:50 +0530

    added a even position elements of an array.

---
+++ src/main/java/com/kashish/tutorial/java/practise/EvenPositionElements.java
@@ -0,0 +1,16 @@
+package com.kashish.tutorial.java.practise;
+
+public class EvenPositionElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int[] arr= {6,8,27,5,43};
+ 
+ for(int i=0; i<arr.length; i++) {
+	 if(i%2==0)
+	 System.out.print(arr[i]+" ");
+ }
+	
+	}
+
+}
