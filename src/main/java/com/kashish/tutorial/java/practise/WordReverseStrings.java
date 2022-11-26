7d930205806f5543f343dc6b74919af4148de4ac
Coder Shaira <kangiakashish2001@gmail.com>
2022-11-26 12:17:44 +0530

    added a word reverse string code.

---
+++ src/main/java/com/kashish/tutorial/java/practise/WordReverseStrings.java
@@ -0,0 +1,14 @@
+package com.kashish.tutorial.java.practise;
+public class WordReverseStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String statement="Hello! I am Shaira.";
+for(int i=statement.length()-1; i>=0; i--) {
+	System.out.print(statement.charAt(i));
+}
+
+
+	}
+
+}
